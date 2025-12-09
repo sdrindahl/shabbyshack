b471eaf08c16c00ee75fd17ac300d2b8e62ffb58
Scott Rindahl <sdrindahl@gmail.com>
2025-12-09 17:38:20 -0600

    updated the index.js file

--- api/index.js
+++ api/index.js
@@ -1,13 +1,16 @@
 const express = require('express');
-
 const app = express();
 
 app.get('/ping', (req, res) => {
   res.json({ message: 'pong' });
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT;
 console.log('process.env.PORT:', process.env.PORT);
+if (!PORT) {
+  console.error('PORT is not set!');
+  process.exit(1);
+}
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
