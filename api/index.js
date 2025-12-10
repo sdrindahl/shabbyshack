2f7341c51de76a2327d9c5b8d7215f0eddc87353
Scott Rindahl <sdrindahl@gmail.com>
2025-12-09 20:14:57 -0600

    change pics to upload bigger size

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 
 // Health check
 app.get('/ping', (req, res) => {
